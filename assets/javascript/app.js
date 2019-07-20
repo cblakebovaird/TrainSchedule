3eb93c44eb4072eefe54a285673bba055d9e01f2
Blake Bovaird <cblakebovaird@gmail.com>
2019-07-20 11:19:35 -0400

    added values to table using firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyA-IqiNClsRtS41AhANnpOkbtcUEUQDiRc",
+  authDomain: "trainschedule-22ea3.firebaseapp.com",
+  databaseURL: "https://trainschedule-22ea3.firebaseio.com",
+  projectId: "trainschedule-22ea3",
+  storageBucket: "",
+  messagingSenderId: "792518542250",
+  appId: "1:792518542250:web:2288a9072dcc787e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
 
 $("#add-train").on("click", function(event) {
     // event.preventDefault() prevents submit button from trying to send a form.
@@ -7,13 +21,49 @@ $("#add-train").on("click", function(event) {
 
     // Write code to grab the text the user types into the input field
     // Write code to add the new animal into the movies array
-    var newTrain = $("#trainNameInput").val();
-    console.log(newTrain);
-
-    // Clears the text field after creating the new button
+    var newTrainName = $("#trainNameInput").val().trim();
+    var newDestination = $("#destinationInput").val().trim();
+    var newTime = $("#firstTrainInput").val().trim();
+    var newFrequency = $("#frequencyInput").val().trim();
+        // Clears the text field after creating the new button
     $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstTrainInput").val("");
+    $("#frequencyInput").val("");
 
-    $("#trainOne").text(newTrain);
+    var newtrain = {
+      name : newTrainName,
+      destination : newDestination,
+      time : newTime,
+      frequency : newFrequency
+    }
+
+    database.ref().push(newtrain);
+
+    console.log(newtrain);
+
+    database.ref().on("child_added", function(childSnapshot){
+      console.log(childSnapshot.val().name);
+      console.log(childSnapshot.val().destination);
+      console.log(childSnapshot.val().time);
+      console.log(childSnapshot.val().frequency);
+
+      var tr = $("<tr>");
+
+      var tdName = $("<td>").text(childSnapshot.val().name);
+      var tdDestination = $("<td>").text(childSnapshot.val().destination);
+      var tdTime = $("<td>").text(childSnapshot.val().time);
+      var tdFrequency = $("<td>").text(childSnapshot.val().frequency);
+
+      tr.append(tdName).append(tdDestination).append(tdTime).append(tdFrequency);
+
+      $(".tbody").append(tr);
+
+
+
+    });
+
+   
 
     // The renderButtons function is called, rendering the list of animal buttons
   });
